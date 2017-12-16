c594b62ab351811c59ef8ccedd696c223932eaba
Mike <cosnmiir@gmail.com>
2017-12-16 22:07:24 +0300

    -

--- Road111/Sys.cs
+++ Road111/Sys.cs
@@ -34,22 +34,14 @@ namespace Road111
         }
         public void writeJ()
         {
-            try
-            {
                 Excel.Application ObjExcel = new Excel.Application();
+                ObjExcel.Visible = true;
                 Excel.Workbook ObjWorkBook;
                 Excel.Worksheet ObjWorkSheet;
                 ObjWorkBook = ObjExcel.Workbooks.Add(System.Reflection.Missing.Value);
                 ObjWorkSheet = (Excel.Worksheet)ObjWorkBook.Sheets[1];
                 ObjWorkSheet.Cells[3, 1] = "51";
-                ObjWorkBook.SaveAs("log.xlsx", Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Excel.XlSaveAsAccessMode.xlExclusive,
-         Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
                 ObjExcel.Quit();
-            }
-            catch (Exception exc)
-            {
-                Console.WriteLine("dfdf");
-            }
         }
     }
 }
